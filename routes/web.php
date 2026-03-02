b6eb15178347d581ceadb396827ce9eb3615accd
warunagit <waruna.git@gmail.com>
2026-03-02 16:57:23 +0200

    v07 13.33

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use \Illuminate\Support\Arr;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/contact', function () {
+    return view('contact');
+});
+
 Route::get('/jobs', function () {
     return view('jobs',[
-        "jobs"=>[
-            [
-                "id"=>1,
-                "title"=>"Director",
-                "salary"=>"50,000"
-            ],
-            [
-                "id"=>2,
-                "title"=>"Designer",
-                "salary"=>"35,000"
-            ],
-            [
-                "id"=>3,
-                "title"=>"Engineer",
-                "salary"=>"45,000"
-            ]
-        ]
+        "jobs"=>Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs=[
-        [
-            "id"=>1,
-            "title"=>"Director",
-            "salary"=>"50,000"
-        ],
-        [
-            "id"=>2,
-            "title"=>"Designer",
-            "salary"=>"35,000"
-        ],
-        [
-            "id"=>3,
-            "title"=>"Engineer",
-            "salary"=>"45,000"
-        ]
-    ];
-
-    $job = Arr::first($jobs,fn ($job) => $job['id'] == $id);
+    $job = Job::find($id);
     //dd($job);
     return view('job',['job'=>$job]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
 });
